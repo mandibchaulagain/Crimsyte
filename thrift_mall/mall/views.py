160b75ed8fc62b5aaf7f8af918abbb8110b2512c
mandibchaulagain <mandibchaulagain8@gmail.com>
2025-05-05 21:41:22 +0545

    some commit

--- thrift_mall/mall/views.py
+++ thrift_mall/mall/views.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from .forms import ContactForm
 
-def home(request):
+def index(request):
     # Create a range for the gallery images (16 images)
     gallery_range = [str(i) for i in range(1, 17)]
-    return render(request, 'mall/home.html', {'gallery_range': gallery_range})
+    return render(request, 'mall/index.html', {'gallery_range': gallery_range})
